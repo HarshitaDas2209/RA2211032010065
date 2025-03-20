cafe19f0459060f427aa1af95f120da32fd0b161
HarshitaDas2209 <130504068+HarshitaDas2209@users.noreply.github.com>
2025-03-20 20:01:27 +0530

    Add files via upload

---
+++ SocialMedia/src/api/mockData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://20.244.56.144/test';
+
+export const fetchUsers = async () => {
+  const response = await axios.get(`${API_BASE_URL}/users`);
+  return response.data.users;
+};
+
+export const fetchUserPosts = async (userId) => {
+  const response = await axios.get(`${API_BASE_URL}/users/${userId}/posts`);
+  return response.data.posts;
+};
+
+export const fetchPostComments = async (postId) => {
+  const response = await axios.get(`${API_BASE_URL}/posts/${postId}/comments`);
+  return response.data.comments;
+};
+
